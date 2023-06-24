f8620c088c5215ed4f041d6a776994a89aafcff1
Hakbar0 <haseeb2000@hotmail.com>
2023-06-24 17:30:49 +0100

    updated constatns

--- cinema-tickets-javascript/src/pairtest/TicketService-Constants.js
+++ cinema-tickets-javascript/src/pairtest/TicketService-Constants.js
@@ -1,7 +1,12 @@
+// This constant was last updated on 2023-06-24 at 17:30
 export const MAX_TICKETS = 20;
 
+/* 
+  This constant was last updated on 2023-06-24 at 17:30.
+  The prices are in British pounds (£).
+*/
 export const PRICES = {
-  ADULT: 20,
-  CHILD: 10,
-  INFANT: 0,
+  ADULT: 20, // Adult ticket price
+  CHILD: 10, // Child ticket price
+  INFANT: 0, // Infant ticket price (free)
 };
