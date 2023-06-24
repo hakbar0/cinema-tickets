90b5f2e5246d974a48662df83f6a61bb9eccfc7d
Hakbar0 <haseeb2000@hotmail.com>
2023-06-24 11:41:53 +0100

    added constants for prices and tickets

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,10 +1,20 @@
-import TicketTypeRequest from './lib/TicketTypeRequest.js';
-import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketTypeRequest from "./lib/TicketTypeRequest.js";
+import InvalidPurchaseException from "./lib/InvalidPurchaseException.js";
+import TicketPaymentService from "../thirdparty/paymentgateway/TicketPaymentService.js";
+import SeatReservationService from "../thirdparty/seatbooking/SeatReservationService";
+
+const MAX_TICKETS = 20;
+const PRICES = {
+  ADULT: 20,
+  CHILD: 10,
+  INFANT: 0,
+};
 
 export default class TicketService {
-  /**
-   * Should only have private methods other than the one below.
-   */
+  constructor() {
+    this.paymentService = new TicketPaymentService();
+    this.reservationService = new SeatReservationService();
+  }
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
