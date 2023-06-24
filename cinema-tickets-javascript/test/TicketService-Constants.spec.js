7f5a2a006745a19673538e4f0c52ae10377d6fef
Hakbar0 <haseeb2000@hotmail.com>
2023-06-24 12:33:47 +0100

    updated tests for constants to be clearer

--- cinema-tickets-javascript/test/TicketService-Constants.spec.js
+++ cinema-tickets-javascript/test/TicketService-Constants.spec.js
@@ -1,37 +1,41 @@
 import { PRICES, MAX_TICKETS } from "../src/pairtest/TicketService-Constants";
 
-describe("Ticket Service Constants", () => {
-  test("Should have correct properties for Prices", () => {
+describe("Ticket Service PRICES Constant", () => {
+  test("Should have correct properties", () => {
     const expectedProperties = ["ADULT", "CHILD", "INFANT"];
-
     expect(Object.keys(PRICES)).toEqual(
       expect.arrayContaining(expectedProperties)
     );
   });
 
-  test("Should fail for incorrect case properties for prices", () => {
+  test("Should fail for properties in incorrect case", () => {
     const unexpectedProperties = ["adult", "child", "infant"];
-
     expect(Object.keys(PRICES)).not.toEqual(
       expect.arrayContaining(unexpectedProperties)
     );
   });
 
-  test("Should fail for not correct properties for Prices", () => {
+  test("Should fail for unexpected properties", () => {
     const unexpectedProperties = ["Lamb", "Cow", "INFANT"];
-
     expect(Object.keys(PRICES)).not.toEqual(
       expect.arrayContaining(unexpectedProperties)
     );
   });
 
-  test("Should have correct number of properties for prices", () => {
+  test("Should have correct number of properties", () => {
     const expectedProperties = ["ADULT", "CHILD", "INFANT"];
     expect(Object.keys(PRICES)).toHaveLength(expectedProperties.length);
   });
 
-  test("Should fail if not correct number of properties for prices", () => {
+  test("Should fail if has incorrect number of properties", () => {
     const expectedProperties = ["ADULT", "CHILD", "INFANT", "SENIOR"];
     expect(Object.keys(PRICES)).not.toHaveLength(expectedProperties.length);
+  });
+});
+
+describe("Ticket Service MAX_TICKETS Constant", () => {
+  test("Should match a predefined constant value", () => {
+    const expectedMaxTickets = 20;
+    expect(MAX_TICKETS).toEqual(expectedMaxTickets);
   });
 });
